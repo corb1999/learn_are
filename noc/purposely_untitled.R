9a1b950e894cabb0769bd6327adecec70047a6d5
corb1999 <le.corbusier.29845@gmail.com>
2022-08-11 16:07:31 -0400

    adding a truly scratchpaper section of this repo to play with one-off things

---
+++ noc/purposely_untitled.R
@@ -0,0 +1,17 @@
+# purely screwing around
+
+
+
+curve(x / (x^3 + 9), -10, 10)
+
+
+cbind(rep(1:6, each = 6), rep(1:6, times = 6))
+
+(0.2 * seq(1000, 1300, 50) + 999) / seq(1000, 1300, 50)
+
+aggregate(mpg ~ cyl + gear, mtcars, mean)
+
+?grep
+
+
+
